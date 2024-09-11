14e79ff63e1efc303c405e8fa9cb10aeeb1fef7e
Florian Francisco <francisco0.florian@gmail.com>
2024-09-11 15:17:24 +0200

    added page error 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Error404 from './pages/error404';
 
 
 export default function app(){
@@ -7,6 +8,7 @@ export default function app(){
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/error404.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Error404() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-4xl text-center">404 - Page not found</h1>
+        </div>
+    )
+}
